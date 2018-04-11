e040ab1dea09895ee757e0f9d8c32207d267e5d8
Ujjaval Sonera <soneraujjaval07@gmail.com>
2018-04-11 15:22:38 +0530

    11-04-2018

--- Hrinnova_FormsProject/Hrinnova_FormsProject/Controllers/FormPopulateController.cs
+++ Hrinnova_FormsProject/Hrinnova_FormsProject/Controllers/FormPopulateController.cs
@@ -75,5 +75,13 @@ namespace Hrinnova_FormsProject.Controllers
             return View(modelobject);
         }
 
+        public ActionResult SUMSEmployeeVerification(int id)
+        {
+            Session["id"] = id;
+            return View(modelobject);
+        }
+
+
+
     }
 }
